375f3d49893cee1e05aa1427a90bf8ebfff4d5f4
Nidhishree <nidhishree@Ranjits-Mac-mini.local>
2019-07-30 16:52:23 +0530

    first project

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+var someInts:[Float] = [10,20,30]
+print(someInts)
+var tuple = (501,"Not implemented")
+print(tuple)
+let const = 42
+print(const)
---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Demoproject
+//
+//  Created by Nidhishree on 30/07/19.
+//  Copyright © 2019 YML. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("This is my demo project")
+    }
+
+
+}
+
